4d5370a50d72237df25477689214981057e355b9
Daniel Vargas <50393869+dv1runner@users.noreply.github.com>
2019-11-02 14:22:00 -0700

    Add files via upload

---
+++ VirtualButtonScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+
+    public class VirtualButtonScript : MonoBehaviour, IVirtualButtonEventHandler
+{
+    public GameObject sphereGo, cubeGo;
+    VirtualButtonBehaviour vrb;
+    // Use this for initialization
+    void Start()
+    {
+        vrb = GetComponentInChildren<VirtualButtonBehaviour>();
+        vrb.RegisterEventHandler(this);
+        cubeGo.SetActive(true);
+        sphereGo.SetActive(false);
+        Debug.Log("Command reached in the expected position");
+    }
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        cubeGo.SetActive(false);
+        sphereGo.SetActive(true);
+    }
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        cubeGo.SetActive(true);
+        sphereGo.SetActive(false);
+    }
+}
+ 
